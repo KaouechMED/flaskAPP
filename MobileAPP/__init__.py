e3dc36458c111a857974b90e6c2b74999b2e05fb
KaouechMED <medkaou12@gmail.com>
2023-11-30 11:38:09 +0100

    requirements update

--- MobileAPP/__init__.py
+++ MobileAPP/__init__.py
@@ -5,7 +5,7 @@ from flask_login import LoginManager
 
 
 app=Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']='mysql://root:@localhost/mobileapp' 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/mobileapp'
 app.config['SECRET_KEY'] = 'ec9439cfc6c796ae2029594d'
 db=SQLAlchemy(app)
 bcrypt=Bcrypt(app)
